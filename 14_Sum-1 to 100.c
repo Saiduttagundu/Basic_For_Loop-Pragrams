1e4be7f6011fc67da418d722a64d84e6774cdd7d
Saidutta Gundu <saidattagundu15@gmail.com>
2025-10-17 22:15:25 +0530

    Add files via upload

---
+++ 14_Sum-1 to 100.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() 
+{
+    int i, sum = 0;
+
+    for(i = 2; i <= 100; i += 2)
+    {
+        sum += i;
+    }
+        
+    printf("Sum of 100 even numbers = %d", sum);
+    return 0;
+}
